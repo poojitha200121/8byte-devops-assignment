bf3e73bbbda13ea776d865ea17fed7755245169b
poojitha.ku@cyraacs.com <poojitha.ku@cyraacs.com>
2026-09-11 17:33:55 +0530

    changes added

--- terraform/infrastructure/backend.tf
+++ terraform/infrastructure/backend.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-
+    bucket       = "8byte-devops-assignment-terraform-state-521223133967"
+    key          = "staging/terraform.tfstate"
+    region       = "ap-south-1"
     encrypt      = true
     use_lockfile = true
-
   }
 }
